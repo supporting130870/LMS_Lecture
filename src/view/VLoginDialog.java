973bdd011afcc540e5783c01b59d34c2096b766d
Jiwon <joung130870@gmail.com>
2024-06-07 02:31:42 +0900

    login capcha

--- src/view/VLoginDialog.java
+++ src/view/VLoginDialog.java
@@ -16,11 +16,13 @@ public class VLoginDialog extends JDialog {
     private JButton signUpButton;
     private DAOUser daoUser;
     private JFrame parent;
-
+    private  int count;
+    private boolean locked;
     public VLoginDialog(JFrame parent) {
         super(parent, "Login", true);
         this.parent = parent;
-
+        count = 0;
+        locked = false;
         daoUser = new DAOUser();
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
@@ -110,6 +112,18 @@ public class VLoginDialog extends JDialog {
     }
 
     private void handleLogin() {
+        if (locked) {
+            String input = JOptionPane.showInputDialog(this, "'나는 사람입니다.'를 입력하세요.");
+            if ("나는 사람입니다.".equals(input)) {
+                count = 0;
+                locked = false;
+                JOptionPane.showMessageDialog(this, "You can try logging in again.", "Information", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(this, "Incorrect phrase. Try again.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+            return;
+        }
+
         String id = idField.getText();
         String password = new String(passwordField.getPassword());
 
@@ -123,10 +137,19 @@ public class VLoginDialog extends JDialog {
                 dispose();
                 ((VMainFrame) parent).initialize();  // 로그인 성공 시 메인 프레임의 initialize 메서드 호출
             } else {
+                count++;
                 JOptionPane.showMessageDialog(this,
                         "Invalid ID or Password",
                         "Error",
                         JOptionPane.ERROR_MESSAGE);
+                if(count >= 5) {
+                    locked = true;
+                    JOptionPane.showMessageDialog(this,
+                            "5회이상 비밀번호를 틀리셨습니다. '나는 사람입니다.'를 타이핑해야 다시 로그인을 할 수 있습니다.",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                    handleLogin();
+                }
             }
         } catch (IOException e) {
             JOptionPane.showMessageDialog(this,
